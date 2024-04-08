76afc6a5df5eda0208ec11334bad4eaa631ce0c3
debdutsahaangel <138654693+debdutsahaangel@users.noreply.github.com>
2024-04-08 12:10:06 +0530

    have supported prisma

---
+++ lib/data/datasources/userDataSources.ts
@@ -0,0 +1,7 @@
+import {IUserDataSources} from '@dataSource/IUserDataSources'
+/*
+    This class is reposible for provide all the data related to user.
+*/
+class UserDataSourceImpl implements IUserDataSources {
+    
+}
